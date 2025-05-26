f2e9aacc4bd84926977f16e3f99190c3bd9672b3
Chris Guimarin <Chris.Guimarin@gmail.com>
2025-05-26 17:31:23 -0400

    Copy edits and fixing css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+    // Pass through copy for CSS files
+    eleventyConfig.addPassthroughCopy("src/style.css");
+
     return {
         dir: {
             input: "src",
